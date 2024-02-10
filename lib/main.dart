2f7e1a04db6b6bb88927b85856dc001257237a0d
LeTrongDuc2510 <leducpro25102003@gmail.com>
2024-02-10 22:23:47 +0700

    khai code

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -16,9 +15,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Hello World',
         theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyanAccent)),
         home: MyHomePage(),
         debugShowCheckedModeBanner: false,
       ),
@@ -28,28 +26,65 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var word = appState.current;
 
     return Scaffold(
-      body: Column(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('This app made by Lee Duck'),
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-
+          Text("Hello world"),
           // ↓ Add this.
+          SizedBox(height: 10),
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext(); // ← This instead of print().
             },
             child: Text('Next'),
           ),
+          SizedBox(height: 10),
+          BigCard(word: word),
         ],
+      ),
+    ));
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.word,
+  });
+
+  final WordPair word;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          word.asLowerCase,
+          style: style,
+          semanticsLabel: "${word.first} ${word.second}",
+        ),
       ),
     );
   }
